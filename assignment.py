9fc1dcb433c6ae7dd6a7ffc1bb801d02ce3508e6
james-wandago <jameswandago@gmail.com>
2026-03-12 19:19:25 +0300

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,28 @@
+ # Question 4
+print("Hello AI Students")
+
+
+# Question 5
+name = "John"
+age = 20
+city = "Nairobi"
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+
+# Question 6
+languages = ["Python", "Java", "C++", "JavaScript", "Go"]
+
+print(languages[0])
+
+
+# Question 7
+student = {
+    "name": "John",
+    "age": 21,
+    "course": "AI"
+}
+
+print(student) 
